30c7126707bc0a8a26d12184de12c7b97858c097
foash-111 <mr2221@fayoum.edu.eg>
2024-08-12 23:29:13 +0300

    subtract 2 col from each other

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,4 @@
 --  a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (split - formed) AS lifespan FROM metal_bands WHERE style = 'Glam rock' AND  split < 2022;
+SELECT band_name, (split - formed) AS lifespan FROM metal_bands
+WHERE style = 'Glam rock' AND  split < 2022
+ORDER BY (split - formed) DESC;
