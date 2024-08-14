b24dc91146c250913796c6ba436cbf41d4e8a20b
foash-111 <mr2221@fayoum.edu.eg>
2024-08-14 03:47:59 +0300

    pymongo insert and find

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""insert into mongo collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    my_dict = {}
+    for k, v in kwargs.items():
+        my_dict[k] = v
+
+    mongo_collection.insert_one(my_dict)
+    return mongo_collection.find(my_dict, {'_id': 1})
