8dd3fdebe451033c5fc258f3990c3982dac8a103
foash-111 <mr2221@fayoum.edu.eg>
2024-08-14 04:39:20 +0300

    pymongo update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""pymongo update"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """update"""
+    mongo_collection.update_one({'name': name}, {'name': topics})
