b432875e9b1dfb022eddd448a0a0a6e1a6309439
foash-111 <mr2221@fayoum.edu.eg>
2024-08-17 16:45:43 +0300

    callable function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,16 +13,15 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float, None]) -> str:
         id = str(uuid4())
         self._redis.mset({id: data})
         return id
 
-    def get(key: str, fn: Optional[Callable] = None) ->\
+    def get(self, key: str, fn: Optional[Callable] = None) ->\
             Union[bytes, int, str, float]:
         """like redis get but return the value like we store it"""
-        r = redis.Redis()
-        value = r.get(key)
+        value = self._redis.get(key)
         if value is None:
             return None
         elif fn is None:
@@ -31,10 +30,10 @@ class Cache:
             converted_value = fn(value)
         return converted_value
 
-    def get_str(value: bytes) -> str:
+    def get_str(self, value: bytes) -> str:
         return str(value)
 
-    def get_int(value: bytes) -> int:
+    def get_int(self, value: bytes) -> int:
         try:
             return int(value)
         except ValueError:
