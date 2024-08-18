f7d0993ab44ba942bd13742ea824fdc05687a15d
foash-111 <mr2221@fayoum.edu.eg>
2024-08-18 16:33:50 +0300

    counter page decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,7 +14,6 @@ def counter(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(url):
         """wrapper"""
-        r.incr(f'count:{url}')
         cached_page = r.get(url)
         if cached_page:
             return cached_page.decode('utf-8')
@@ -29,4 +28,5 @@ def counter(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """get the page data from its url"""
     response = requests.get(url)
+    r.incr(f'count:{url}')
     return response.text
