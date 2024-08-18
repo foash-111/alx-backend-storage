871e0d55b8f83e80417f952f692d84f1d084561a
foash-111 <mr2221@fayoum.edu.eg>
2024-08-18 15:19:06 +0300

    counter page decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,17 +6,27 @@ from typing import Callable
 from functools import wraps
 
 
+r = redis.Redis()
+
 def counter(method: Callable) -> Callable:
     """counter decorator"""
     @wraps(method)
     def wrapper(*args, **kwargs):
         """wrapper"""
-        r = redis.Redis()
         url = args[0]
-        if not r.exists(f'count:{url}'):
-            r.setex(f'count:{url}', 10, 0)
+        # if not r.exists(f'count:{url}'):
+        #     r.setex(f'count:{url}', 10, 0)
+        # r.incr(f'count:{url}')
+
         r.incr(f'count:{url}')
-        return method(*args, **kwargs)
+        cashed_data = r.get(f'{url}')
+        if cashed_data:
+            return cashed_data.decode('utf-8')
+        
+        
+        html_page =  method(*args, **kwargs)
+        r.setex(f'{url}', 10, html_page)
+        return html_page
     return wrapper
 
 
@@ -24,8 +34,7 @@ def counter(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """get the page data from its url"""
     response = requests.get(url)
-    data = response.text
-    return data
+    return response.text
 
 
-print(get_page('http://slowwly.robertomurray.co.uk'))
+print(get_page('http://127.0.0.1:80'))
