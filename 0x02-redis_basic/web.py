beba158f420008931dd79f3f16d4cafc218afed3
foash-111 <mr2221@fayoum.edu.eg>
2024-08-18 16:47:52 +0300

    counter page decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,16 +10,23 @@ r = redis.Redis()
 
 
 def counter(method: Callable) -> Callable:
-    """counter decorator"""
+    """Counter decorator to cache the result and count requests"""
     @wraps(method)
-    def wrapper(url):
-        """wrapper"""
+    def wrapper(url: str):
+        """Wrapper function"""
+        # Increment the count for the given URL
+        r.incr(f'count:{url}')
+        
+        # Check if the content is already cached
         cached_page = r.get(url)
         if cached_page:
+            print(f"Returning cached page for {url}")
             return cached_page.decode('utf-8')
 
+        # Fetch and cache the page content
         html_page = method(url)
         r.setex(url, 10, html_page)
+        print(f"Cached page for {url} for 10 seconds")
         return html_page
     return wrapper
 
@@ -28,5 +35,4 @@ def counter(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """get the page data from its url"""
     response = requests.get(url)
-    r.incr(f'count:{url}')
     return response.text
